7d237df21eb3364608250885eb8638f420e8bad4
Joseph Kobrosky <jkobrosky@gmail.com>
2015-05-27 23:01:19 -0600

    Completed project

---
+++ app.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	var color = 'white';
+	var colors = 'white red green blue yellow';
+
+	$('.box').on('click', function() {
+		$(this).addClass('white');
+	})
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass('white');
+	})
+
+	$('#reset').on('click', function() {
+		$('.box').removeClass('white');
+	})
+
+	$('#red').on('click', function() {
+		color = 'red';
+	})
+
+	$('#green').on('click', function() {
+		color = 'green';
+	})
+
+	$('#blue').on('click', function() {
+		color = 'blue';
+	})
+
+	$('#yellow').on('click', function() {
+		color = 'yellow';
+	})
+
+	$('#white').on('click', function() {
+		color = 'white';
+	})
+
+	$('.box').on('click', function() {
+		$(this).addClass(color);
+	})
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(color);
+	})
+
+	$('#reset').on('click', function() {
+		$('.box').removeClass(color);
+	})
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(colors);
+	})
+
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
+	})
+
+});
